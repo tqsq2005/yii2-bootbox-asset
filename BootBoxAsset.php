20c005c7f2333f2af2561fca8daafbef40cf0975
Dmitriy Brusenskiy <brusenskiydmitriy@gmail.com>
2015-11-24 04:05:32 +0300

    New version 0.1.2

--- BootBoxAsset.php
+++ BootBoxAsset.php
@@ -3,7 +3,7 @@
  * Asset for bootbox plugin
  * @author Brusenskiy Dmitry <brussens@nativeweb.ru>
  * @since 0.1.0
- * @version 0.1.1
+ * @version 0.1.2
  * @link https://github.com/yiiassets/yii2-bootbox-asset
  * @link http://bootboxjs.com/
  * @copyright 2015 Brusenskiy Dmitry
